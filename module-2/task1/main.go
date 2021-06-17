5cfbcb12d0c7ed2350b6a36f1a2807616977634a
Pawel Bochniak <pawel_bochniak@epam.com>
2021-06-17 09:30:25 +0200

    fix PR comment

--- module-2/task1/main.go
+++ module-2/task1/main.go
@@ -7,21 +7,27 @@ import (
 	"github.com/pbochniak/computator/fibonacci"
 )
 
+func castNumber(input string) (interface{}, error) {
+	if u, err := strconv.ParseUint(input, 10, 0); err == nil {
+		return uint(u), nil
+	}
+	if i, err := strconv.ParseInt(input, 10, 0); err == nil {
+		return int(i), nil
+	}
+	if f, err := strconv.ParseFloat(input, 64); err == nil {
+		return f, nil
+	}
+	return nil, fmt.Errorf("invalid type")
+}
+
 func main() {
 	var inputNumber string
-	var number interface{}
 	fmt.Printf("Give me a number: ")
 	fmt.Scanf("%s", &inputNumber)
-	if u, err := strconv.ParseUint(inputNumber, 10, 0); err == nil {
-		number = uint(u)
-	} else if i, err := strconv.ParseInt(inputNumber, 10, 0); err == nil {
-		number = int(i)
-	} else if f, err := strconv.ParseFloat(inputNumber, 64); err == nil {
-		number = f
-	} else {
-		fmt.Printf("Error: %q isn't a number.\n", inputNumber)
+	number, err := castNumber(inputNumber)
+	if err != nil {
+		fmt.Printf("Error: %q isn't a correct number.\n", inputNumber)
+		return
 	}
-	if number != nil {
-		fmt.Printf("Fibonacci sequence for given number (%v [%T]) equals %v\n", number, number, fibonacci.Fibonacci(number))
-	}
+	fmt.Printf("Fibonacci sequence for given number (%v [%T]) equals %v\n", number, number, fibonacci.Fibonacci(number))
 }
