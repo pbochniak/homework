ad8c0a7201adafd34dfe41eea836ec23b5675641
Pawel Bochniak <pawel_bochniak@epam.com>
2021-06-20 10:40:00 +0200

    [Module #3] solved task2 - own sorting

--- module-3/task2/slices/slice.go
+++ module-3/task2/slices/slice.go
@@ -1,10 +1,20 @@
 package slices
 
-func SortStringsInPlace(s []string) {
+func SortStringsInPlace(strings []string) {
+	for i := 0; i < len(strings); i++ {
+		for j := i + 1; j < len(strings); j++ {
+			if strings[i] > strings[j] {
+				strings[i], strings[j] = strings[j], strings[i]
+			}
+		}
+	}
 }
 
-func SortStringsPure(s []string) []string {
-
+func SortStringsPure(strings []string) []string {
+	temp_strings := make([]string, len(strings))
+	copy(temp_strings, strings)
+	SortStringsInPlace(temp_strings)
+	return temp_strings
 }
 
 type User struct {
@@ -12,10 +22,44 @@ type User struct {
 	LastName  string
 }
 
-func SortUsersPure(s []User) []User {
-	return r
+func (that User) Compare(another User) int {
+	if that.FirstName > another.FirstName {
+		return 1
+	}
+	if that.FirstName < another.FirstName {
+		return -1
+	}
+	if that.LastName > another.LastName {
+		return 1
+	}
+	if that.LastName < another.LastName {
+		return -1
+	}
+	return 0
 }
 
-func SortUsersPureDesc(s []User) []User {
-	return r
+func SortUsersPure(users []User) []User {
+	temp_users := make([]User, len(users))
+	copy(temp_users, users)
+	for i := 0; i < len(temp_users); i++ {
+		for j := i + 1; j < len(temp_users); j++ {
+			if temp_users[i].Compare(temp_users[j]) > 0 {
+				temp_users[i], temp_users[j] = temp_users[j], temp_users[i]
+			}
+		}
+	}
+	return temp_users
+}
+
+func SortUsersPureDesc(users []User) []User {
+	temp_users := make([]User, len(users))
+	copy(temp_users, users)
+	for i := 0; i < len(temp_users); i++ {
+		for j := i + 1; j < len(temp_users); j++ {
+			if temp_users[i].Compare(temp_users[j]) < 0 {
+				temp_users[i], temp_users[j] = temp_users[j], temp_users[i]
+			}
+		}
+	}
+	return temp_users
 }
