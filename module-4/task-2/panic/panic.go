198beb471f79aef28bffd0e5892872f7e57f48dd
Pawel Bochniak <pawel_bochniak@epam.com>
2021-06-27 12:46:16 +0200

    [Module #4] solved task2

--- module-4/task-2/panic/panic.go
+++ module-4/task-2/panic/panic.go
@@ -1,9 +1,18 @@
 package panic
 
+import "fmt"
+
 func iWillPanic() {
 	panic("something")
 }
 
+func recoveryPanic() {
+	if err := recover(); err != nil {
+		fmt.Printf("recovery from panic - %q\n", err)
+	}
+}
+
 func catchPanic() {
+	defer recoveryPanic()
 	iWillPanic()
 }
