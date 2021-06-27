5a6915698e533069ff1d00ee5bea3c6d4e8bcd9b
Pawel Bochniak <pawel_bochniak@epam.com>
2021-06-27 12:38:05 +0200

    [Module #4] solved task1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pbochniak
+
+go 1.16
--- module-4/task-1/palindrome/palindrome.go
+++ module-4/task-1/palindrome/palindrome.go
@@ -1,5 +1,13 @@
 package palindrome
 
+import "unicode"
+
 func IsPalindrome(s string) bool {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < len(runes)/2; i, j = i+1, j-1 {
+		if unicode.ToLower(runes[i]) != unicode.ToLower(runes[j]) {
+			return false
+		}
+	}
 	return true
 }
